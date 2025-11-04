461d7a909f2f3117142184e5508143151a924573
Ameerlog <testbatteryfy@gmail.com>
2025-11-04 12:27:14 +0530

    Fix blank page - base path and vercel rewrite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,4 +12,5 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
+ base: '/',
 })
